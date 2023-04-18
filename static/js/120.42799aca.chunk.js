be901e3f347d8195f98a1acb84c4e60974a82e0c
NASTYAPC <aribragimov@inbox.ru>
2023-04-18 21:18:44 +0300

    Updates

---
+++ static/js/120.42799aca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[120],{7562:function(e,n,u){u.r(n);var p=u(184);n.default=function(e){return(0,p.jsx)("div",{children:"\u041b\u0415\u041d\u0422\u0410"})}}}]);
+//# sourceMappingURL=120.42799aca.chunk.js.map
